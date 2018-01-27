8c104d20ff2b06dbcdeafc7e169a6a44b2bb912b
xmoyking <xmoyking@gmail.com>
2018-01-27 23:15:47 +0800

    init and simple demo

---
+++ app/src/components/TodoAdd.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="addTodo">
+      <input 
+        type="text"
+        name=""
+        class="input"
+        value=""
+        v-model="todo"
+        @keyup.enter="addTodo"
+      >
+      <button
+        type="button"
+        name="button"
+        @click="addTodo"
+      >
+        Add
+      </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'addTodo',
+  data: () => ({
+      todo: ''
+  }),
+  methods:{
+      addTodo(){
+          if(this.todo){
+              this.$emit('add', this.todo);
+              this.todo = ''
+          }
+      }
+  }
+}
+</script>
+
+<style scoped>
+.input{
+    min-width: 200px;
+}
+</style>
---
+++ app/src/components/TodoList.vue
@@ -0,0 +1,89 @@
+<template>
+  <div id="todoList">
+    <h1>Todo List</h1>
+    <todo-add v-on:add="addTodo"></todo-add>
+    <ul class="todos">
+      <li v-for="(todo, index) in todos" class="todo">
+        <input
+          type="checkbox"
+          name=""
+          value=""
+          :checked="todo.isCompleted"
+          @click="completed(index)"
+        >
+        <span
+          :class="todo.isCompleted ? 'completed' : ''"
+          @click="completed(index)"
+        >
+          <em>{{ index + 1 }}.</em>{{ todo.text }}
+        </span>
+      </li>
+    </ul>
+    <div>
+      <p v-show="todos.length === 0">All is done</p>
+      <p v-show="todos.length !== 0">
+          <strong>{{todos.length}}</strong> task to do,
+          {{completedCounts}} have done.
+          {{notCompletedCounts}} not do.
+      </p>
+  </div>
+  </div>
+</template>
+
+<script>
+import TodoAdd from './TodoAdd.vue';
+export default {
+  name: 'TodoList',
+  components: {
+      TodoAdd
+  },
+  data: ()=>({
+      todos: [{
+          text: 'todo1',
+          isCompleted: false
+      },{
+          text: 'todo2',
+          isCompleted: false
+      }]
+  }),
+  methods:{
+      completed(index){
+          this.todos[index].isCompleted = !this.todos[index].isCompleted;
+      },
+      addTodo(todo){
+          this.todos.push({
+              text: todo,
+              isCompleted: false
+          })
+      }
+  },
+  computed: {
+      completedCounts(){
+          return this.todos.filter(item => item.isCompleted).length;
+      },
+      notCompletedCounts(){
+          return this.todos.filter(item => !item.isCompleted).length;
+      }
+  }
+}
+</script>
+
+<style scoped>
+#todoList{
+    margin: 0 auto;
+    max-width: 350px;
+}
+
+.todos li{
+    list-style: none;
+}
+
+.todo{
+    text-align: left;
+    cursor: pointer;
+}
+
+.completed{
+    text-decoration: line-through;
+}
+</style>
