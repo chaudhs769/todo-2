b31927417327a35476fb07db5c9912bef9673dec
xmoyking <xmoyking@gmail.com>
2018-02-18 22:54:05 +0800

    indexeded 接口

--- app/src/store/state.js
+++ app/src/store/state.js
@@ -1,32 +1,4 @@
 export default {
-    // 用户列表
-    users: {
-        name: 'users',
-        userList: [
-            {
-                name: 'Admin',
-                coding: '000',
-                position: '管理员',
-                password: 'admin',
-                jurisdiction: '一级',
-                telephone: '###########',
-                address: '############',
-                avatar: 'http://img.qq1234.org/uploads/allimg/150128/20123A027-5.jpg'
-            }
-        ]
-    },
-    // 消息通知列表
-    messages: {
-        name: 'messages',
-        today: '2017-11-12',
-        name: 'messages',
-        number: 0,
-        // 保质期
-        limitDate: 30,
-        // 库存
-        limitNumber: 3,
-        messageList: []
-    },
     // 待办事件列表
     todo: {
         name: 'todo',
